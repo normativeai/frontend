ddc3469ea5730fe7fb97b0b7836d9fdc65bd0efd
Alexander Steen <alx.steen@gmail.com>
2019-03-05 12:11:22 +0100

    typo fixed

--- src/vue_app.js
+++ src/vue_app.js
@@ -54,7 +54,7 @@ nai = new function () { var lib = this;
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        if (!!onNoReponse) { onNoResponse(error.request); }
+        if (!!onNoResponse) { onNoResponse(error.request); }
         else { console.log(error.request) }
       } else {
         // Something happened in setting up the request that triggered an Error

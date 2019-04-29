55a828ce3bd16546a17372ec52386080de4dd77e
Alexander Steen <a.steen@fu-berlin.de>
2019-04-29 11:26:00 -0700

    fixed response fading bug (it would change size while fading)

--- src/sites/theory.js
+++ src/sites/theory.js
@@ -459,8 +459,8 @@ const theory = {
           </div>
           <p class="small"><em>A consistency check should be conducted prior to executing any further queries.</em></p>
 
-          <alert v-on:dismiss="consistencyResponse = {};" :variant="consistencyResponse.type" v-show="consistencyResponse.show" :timeout="consistencyResponse.timeout"><span v-html="consistencyResponse.message"></span></alert>
-          <alert v-on:dismiss="independenceResponse = {};" :variant="independenceResponse.type" v-show="independenceResponse.show" :timeout="independenceResponse.timeout"><span v-html="independenceResponse.message"></span></alert>
+          <alert v-on:dismiss="consistencyResponse.show = false;" :variant="consistencyResponse.type" v-show="consistencyResponse.show" :timeout="consistencyResponse.timeout"><span v-html="consistencyResponse.message"></span></alert>
+          <alert v-on:dismiss="independenceResponse.show = false;" :variant="independenceResponse.type" v-show="independenceResponse.show" :timeout="independenceResponse.timeout"><span v-html="independenceResponse.message"></span></alert>
 
           <div class="table-responsive">
             <table class="table table-striped table-sm table-hover" style="table-layout:fixed;">

d7fad9dc6b6d592c84764b9ec0f1fee1a7c39417
gjgoerke <gavingoerke@icloud.com>
2019-07-09 15:01:23 +0200

    Added undo redo buttons, buttons undo text as well (not just annotation, I think this could be changed.) Feature probably needs to be tested a bit more before being merged.#48

--- src/components.js
+++ src/components.js
@@ -715,7 +715,26 @@ Vue.component('quill', {
     return {
       quill: null,
       content0: '',
-      options: { theme: "snow", modules: {toolbar: '#quilltoolbar'} },
+      options: {
+        theme: "snow",
+        modules: {
+          history: {
+            delay: 1000,
+            userOnly: false
+          },
+          toolbar: {
+           container: '#quilltoolbar',
+           handlers: {
+             'undo': function() {
+               this.quill.history.undo();
+             },
+             'redo': function() {
+               this.quill.history.redo();
+             }
+           }
+         }
+         }
+       },
       termPrompt: false, termPromptData: null,
     }
   },
@@ -928,6 +947,20 @@ Vue.component('quill', {
         <span class="ql-formats">
           <button class="ql-script" value="sub" title="Subscript"></button>
           <button class="ql-script" value="super" title="Superscript"></button>
+        </span>
+        <span class="ql-formats">
+          <button class="ql-undo" title="Undo">
+            <svg viewbox="0 0 18 18">
+              <polygon class="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10"></polygon>
+              <path class="ql-stroke" d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"></path>
+            </svg>
+          </button>
+          <button class="ql-redo" title="Redo">
+            <svg viewbox="0 0 18 18">
+              <polygon class="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10"></polygon>
+              <path class="ql-stroke" d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"></path>
+            </svg>
+          </button>
         </span>
         <span class="ql-formats float-right">
           <button type="button" class="btn btn-primary-outline btn-sm mr-4" :disabled="annotateButtonsDisabled"

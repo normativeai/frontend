11124c761013c1d5a8f909057eb0004fe5577638
gjgoerke <gavingoerke@icloud.com>
2019-07-19 15:33:54 +0200

    Fixed bug where svg in redo button expanded to the size of the toolbar.

--- src/components.js
+++ src/components.js
@@ -949,14 +949,14 @@ Vue.component('quill', {
           <button class="ql-script" value="super" title="Superscript"></button>
         </span>
         <span class="ql-formats">
-          <button class="ql-undo" title="Undo">
-            <svg viewbox="0 0 18 18">
+          <button class="ql-undo position-relative" title="Undo">
+            <svg style="width: 100%; height: 100%;" viewbox="0 0 18 18">
               <polygon class="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10"></polygon>
               <path class="ql-stroke" d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"></path>
             </svg>
           </button>
-          <button class="ql-redo" title="Redo">
-            <svg viewbox="0 0 18 18">
+          <button class="ql-redo position-relative" title="Redo">
+            <svg style="width: 100%; height: 100%;" viewbox="0 0 18 18">
               <polygon class="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10"></polygon>
               <path class="ql-stroke" d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"></path>
             </svg>
